b00b9d51e1b77d5f1890afdc477f543d8a6b7311
Mauro Campilongo <120427661+maurocampilongo@users.noreply.github.com>
2023-05-17 18:16:27 -0300

    Corregí los errores que encontramos en el Formulario.

--- js/formulario.js
+++ js/formulario.js
@@ -10,39 +10,47 @@ textarea.addEventListener("input", () => {
 /* Constante para acceder al Formulario de nuestro HTML 
 y a todos los inputs que hay dentro del formulario */
 const formulario = document.getElementById("formulario");
-const inputs = document.querySelectorAll("#formulario input");
+const inputs = document.querySelectorAll("#formulario input, #formulario textarea");
 
 /* El objeto expresiones tienen propiedades que son para cada campo del
 Formulario usando Expresiones Regulares. Usar el mismo nombre
 que se encuentra en el Id HTML para acceder a ellos. */
 const expresiones = {
- nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
- apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
- correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+ nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos. Tope 40 caracteres.
+ apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos. Tope 40 caracteres.
+ correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, 
  telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+ consulta: /^[\s\S]{1,500}$/ //Tope de 500 caracteres
 }
 
+/* Esta parte hace que al ingresar al formulario, por defecto, los campos sean falsos.
+Por lo tanto, no se podrá enviar y saldrá un mensaje de error. */
 const campos = {
  nombre: false,
  apellido: false,
  correo: false,
- telefono: false
+ telefono: false,
+ consulta: false,
 }
+
 /*Función para validar los campos del formulario.
 Hay que usar el "name=" de cada <input> para acceder a ellos.*/
 const validarFormulario = (e) => {
 	switch (e.target.name) {
-		case "nombre":
-			validarCampo(expresiones.nombre, e.target, "nombre");
+	  case "nombre":
+		validarCampo(expresiones.nombre, e.target, "nombre");
 		break;
-		case "apellido":
-			validarCampo(expresiones.apellido, e.target, "apellido");
+	  case "apellido":
+		validarCampo(expresiones.apellido, e.target, "apellido");
 		break;
-		case "correo":
-			validarCampo(expresiones.correo, e.target, "correo");
+	  case "correo":
+		validarCampo(expresiones.correo, e.target, "correo");
 		break;
-		case "telefono":
-			validarCampo(expresiones.telefono, e.target, "telefono");
+	  case "telefono":
+		validarCampo(expresiones.telefono, e.target, "telefono");
+		break;
+	  case "consulta":
+		validarCampo(expresiones.consulta, e.target, "consulta");
 		break;
 	}
 }
@@ -54,7 +62,10 @@ const validarCampo = (expresion, input, campo) => {
 		document.getElementById(`grupo_${campo}`).classList.add("formulario_grupo-correcto");
 		document.querySelector(`#grupo_${campo} i`).classList.add("fa-check-circle");
 		document.querySelector(`#grupo_${campo} i`).classList.remove("fa-times-circle");
-		document.querySelector(`#grupo_${campo} .formulario_input-error`).classList.remove("formulario_input-error-activo");
+		const errorElement = document.querySelector(`#grupo_${campo} .formulario_input-error`);
+			if (errorElement) {
+				errorElement.classList.remove("formulario_input-error-activo");
+			}
 		campos[campo] = true;
 	} else {
 		document.getElementById(`grupo_${campo}`).classList.add("formulario_grupo-incorrecto");
@@ -65,20 +76,18 @@ const validarCampo = (expresion, input, campo) => {
 		campos[campo] = false;
 	}
 }
-
-/* Por cada entrada, ejecuta un código que va validando lo que se escribe.
-O sea, cada vez que el usuario teclea algo, va validando, o bien,
-cuándo deja de hacerlo hace un último chequeo*/
+/* Por cada entrada, ejecuta un código que va validando lo que se escribe. O sea, cada vez que
+el usuario teclea algo, va validando, o bien, cuándo deja de hacerlo hace un último chequeo*/
 inputs.forEach((input) => {
 	input.addEventListener("keyup", validarFormulario); //Evento para cuando se presione una tecla
 	input.addEventListener("blur", validarFormulario); //Evento para cuando se deje de presionar una tecla
 });
 
-/*Evento para cuando se haga click en el boton de enviar el Formulario
+/* Evento para cuando se haga click en el boton de enviar el Formulario
 Se valida que todos los campos esten llenos y que no haya errores*/
 formulario.addEventListener("submit", (e) => { //e es el evento
 	e.preventDefault(); //Evita que se envie el Formulario
-	if(campos.nombre && campos.apellido && campos.correo && campos.telefono){
+	if(campos.nombre && campos.apellido && campos.correo && campos.telefono && campos.consulta){
 		formulario.reset();
 		document.getElementById("formulario_mensaje-exito").classList.add("formulario_mensaje-exito-activo");
 		setTimeout(() => {
@@ -90,4 +99,29 @@ formulario.addEventListener("submit", (e) => { //e es el evento
 	} else {
 		document.getElementById("formulario_mensaje").classList.add("formulario_mensaje-activo");
 	}
+});
+
+/*Evento para que cuando se haga click en el boton borrar del formulario,
+limpie el formulario y los iconos*/
+document.getElementById("boton_reset").addEventListener("click", () => {
+    formulario.reset(); // Restablece los campos del formulario
+
+    // Elimina las clases y mensajes de éxito o error
+    document.querySelectorAll(".formulario_grupo").forEach((grupo) => {
+        grupo.classList.remove("formulario_grupo-incorrecto");
+        grupo.classList.remove("formulario_grupo-correcto");
+    });
+
+    document.querySelectorAll(".formulario_input-error").forEach((error) => {
+        error.classList.remove("formulario_input-error-activo");
+    });
+
+    document.querySelectorAll(".formulario_validacion-estado").forEach((estado) => {
+        estado.classList.remove("fa-times-circle");
+        estado.classList.remove("fa-check-circle");
+    });
+
+    document.getElementById("formulario_mensaje").classList.remove("formulario_mensaje-activo");
+    document.getElementById("formulario_mensaje-exito").classList.remove("formulario_mensaje-exito-activo");
+    document.getElementById("formulario_mensaje-borrado").classList.remove("formulario_mensaje-borrado-activo");
 });
