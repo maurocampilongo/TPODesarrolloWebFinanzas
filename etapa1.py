669c528b44d1d1c18df51d13c22cb81d469a9833
Bibiq <54219062+Bibiq@users.noreply.github.com>
2023-06-28 20:16:10 -0300

    Add files via upload

--- etapa1.py
+++ etapa1.py
@@ -1,306 +1,238 @@
 # -------------------------------------------------------------------
 # Escribimos una serie de funciones para crear una pequeña app que
-# maneje un arreglo que contenga datos de operacions.
-# Cada operacion tiene un código numérico entero, una descripción
-# alfabetica, una tipo en stock y un monto de venta.
+# maneje un arreglo que contenga datos de registros de entradas y salidas de dinero.
+# Cada registro tiene un código numérico entero (id), una fecha, un detalle,
+# una categoría, un rubro y un monto.
+#
 # Nuestras funciones haran lo siguiente:
 #
-# - Agregar un operacion al arreglo
-# - Consultar un operacion a partir de su código
-# - Modificar los datos de un operacion a partir de su código
-# - Obtener un listado de los operacions que existen en el arreglo.
-# - Eliminar un operacion del arreglo
+# - Agregar un registro al arreglo
+# - Consultar un registro a partir de su código
+# - Modificar los datos de un registro a partir de su código
+# - Obtener un listado de los registros que existen en el arreglo.
+# - Eliminar un registro del arreglo
 # -------------------------------------------------------------------
 
-# Definimos una lista para almacenar los operacions.
+# Definimos una lista para almacenar los registros.
 # Es una lista de diccionarios.
-operaciones = []
-# Definir un segundo arreglo para el presupuesto de compras
-presupuesto = []
+registros = []
+# Definir un segundo arreglo para el saldo
+saldo = []
+# Definimos una lista acotada de opciones de categorías
+categoria = ['Ingreso Fijo', 'Ingreso Variable', 'Egreso Fijo', 'Egreso Variable', 'Ahorro']
 
 # -------------------------------------------------------------------
-# Función para agregar un operacion al arreglo
+# Función para agregar un registro al arreglo
 # -------------------------------------------------------------------
-def agregar_operacion(codigo, descripcion, tipo, monto):
+def agregar_registro(codigo, fecha, detalle, categoria, rubro, monto):
     """
-    Agrega un operacion al arreglo de operacions.
+    Agrega un registro al arreglo de registros.
 
     Parámetros:
-    - codigo: int, código numérico del operacion.
-    - tipo: ingreso o egreso
-    - descripcion: str, descripción alfabética del operacion.
+    - codigo: int, código numérico del registro.
+    - fecha: date, fecha del registro 
+    - detalle: string, acá se carga de que se trata el registro
+    - categoria: lista (entrada o salida)
+    - rubro: str, descripción alfabética del registro
     - monto: float
     """
-    # Creamos un diccionario con los datos del operacion...
-    nueva_operacion = {
+    # Creamos un diccionario con los datos del registro...
+    nuevo_registro = {
         'codigo': codigo,
-        'descripcion': descripcion,
-        'tipo': tipo,
+        'fecha': fecha,
+        'detalle': detalle,
+        'categoria': categoria,
+        'rubro': rubro,
         'monto': monto
     }
     # Y lo agregamos a nuestro arreglo.
-    operaciones.append(nueva_operacion)
+    registros.append(nuevo_registro)
+    print(f"Se ha creado el registro {codigo}.")
     return True
 
 # -------------------------------------------------------------------
-# Función para consultar un operacion a partir de su código
+# Función para consultar un registro a partir de su código
 # -------------------------------------------------------------------
-def consultar_operacion(codigo):
+def consultar_registro(codigo):
     """
-    Consulta una operacion a partir de su código y devuelve sus datos.
+    Consulta una registro a partir de su código y devuelve sus datos.
 
     Parámetros:
-    - codigo: int, código numérico del operacion.
+    - codigo: int, código numérico del registro.
 
     Retorna:
-    - dict: datos del operacion en forma de diccionario, o False si no se encontró la operacion.
+    - dict: datos del registro en forma de diccionario, o False si no se encontró la registro.
     """
-    # Recorremos la lista de operacions...
-    for operacion in operaciones:
+    # Recorremos la lista de registros...
+    for registro in registros:
         # Y si el código es el correcto,
-        if operacion['codigo'] == codigo:
+        if registro['codigo'] == codigo:
             # Refresamos el diccionario correspondiente.
-            return operacion
-    # Si el bucle finaliza sin encontrar el operacion,
+            return registro
+    # Si el bucle finaliza sin encontrar el registro,
     # regresamos "falso."
     return False
 
 
 # -------------------------------------------------------------------
-# Función para modificar los datos de un operacion a partir de su código
+# Función para modificar los datos de un registro a partir de su código
 # -------------------------------------------------------------------
-def modificar_operacion(codigo, nueva_descripcion, nuevo_tipo, nuevo_monto):
+def modificar_registro(codigo, new_fecha, new_detalle, new_categoria, new_rubro, new_monto):
     """
-    Modifica los datos de un operacion a partir de su código.
+    Modifica los datos de un registro a partir de su código.
 
     Parámetros:
-    - codigo: int, código numérico del operacion.
-    - nueva_descripcion: str, nueva descripción alfabética del operacion.
-    - nuevo_tipo: str, nueva descripción alfabética del operacion.
-    - nuevo_monto: float, nuevo monto de venta del operacion.
+    - codigo: int, código numérico del registro.
+    - fecha: date, fecha del registro 
+    - detalle: string, acá se carga de que se trata el registro
+    - categoria: lista (entrada o salida)
+    - rubro: str, descripción alfabética del registro
+    - monto: float
     """
-    # Recorremos la lista de operacions...
-    for operacion in operaciones:
+    # Recorremos la lista de registros...
+    for registro in registros:
         # Y si el código es el correcto,
-        if operacion['codigo'] == codigo:
+        if registro['codigo'] == codigo:
             # ...actualizamos los valores de cada clave del diccionario
-            operacion['descripcion'] = nueva_descripcion
-            operacion['tipo'] = nuevo_tipo
-            operacion['monto'] = nuevo_monto
-
-            # Como no hay otro operacion con ese código, salimos del bucle.
+            registro['fecha'] = new_fecha
+            registro['detalle'] = new_detalle
+            registro['categoria'] = new_categoria
+            registro['rubro'] = new_rubro
+            registro['monto'] = new_monto
+            
+            print(f"Se ha modificado el registro {codigo}.")
+            # Como no hay otro registro con ese código, salimos del bucle.
             return True
-    # Si llegamos aqui, el operacion no existe.
+    # Si llegamos aqui, el registro no existe.
     return False
 
 # -------------------------------------------------------------------
-# Función para obtener un listado de los operacions en pantalla
+# Función para obtener un listado de los registros en pantalla
 # -------------------------------------------------------------------
-def listar_operaciones():
+def listar_registros():
     """
-    Muestra en pantalla un listado de las operaciones existentes.
+    Muestra en pantalla un listado de las registros existentes.
     """
-    # Recorremos la lista de operacions...
+    # Recorremos la lista de registros...
     print("-"*30)
-    for operacion in operaciones:
+    print("Listado de Registros")
+    print("-"*30)
+    for registro in registros:
         # Y mostramos los datos de cada uno de ellos.
-        print(f"Código: {operacion['codigo']}")
-        print(f"Descripción: {operacion['descripcion']}")
-        print(f"tipo: {operacion['tipo']}")
-        print(f"monto: {operacion['monto']}")
-        print("-"*30)
+        print(f"Código: {registro['codigo']}")
+        print(f"Fecha: {registro['fecha']}")
+        print(f"Detalle: {registro['detalle']}")
+        print(f"Categoría: {registro['categoria']}")
+        print(f"Rubro: {registro['rubro']}")
+        print(f"Monto: {registro['monto']}")
+        print("-" * 30)
 
 
 # -------------------------------------------------------------------
-# Función para modificar los eliminar un operacion a partir de su código
+# Función para modificar los eliminar un registro a partir de su código
 # -------------------------------------------------------------------
-def eliminar_operacion(codigo):
+def eliminar_registro(codigo):
     """
-    Elimina una operacion a partir de su código.
+    Elimina una registro a partir de su código.
 
     Parámetros:
-    - codigo: int, código numérico de la operacion.
+    - codigo: int, código numérico de la registro.
     """
-    # Recorremos la lista de operacions...
-    for operacion in operaciones:
+    # Recorremos la lista de registros...
+    for registro in registros:
         # Y si el código es el correcto,
-        if operacion['codigo'] == codigo:
+        if registro['codigo'] == codigo:
             # ...lo quitamos de la lista.
-            operaciones.remove(operacion)
-            # Como no hay otro operacion con ese código, salimos del bucle.
+            registros.remove(registro)
+            print(f"Se ha eliminado el registro {codigo}.")
+            # Como no hay otro registro con ese código, salimos del bucle.
             return True
-    # Si llegamos aqui, el operacion no existe.
+    # Si llegamos aqui, el registro no existe.
+    print(f"No se encontró un registro con el código {codigo}.")
     return False
 
-
 # -------------------------------------------------------------------
-# Funciones para el manejo del "presupuesto"
+# Funciones para el manejo del "saldo"
 #
-# Con estas funciones, podrás agregar y quitar operacions al presupuesto
-# y mostrar el contenido del presupuesto en pantalla.
-# Se realizan las verificaciones necesarias para asegurarse de que
-# el operacion exista en el arreglo de operacions y que la tipo en
-# stock sea suficiente.
-#
-# Los diccionarios que representan los elementos en el presupuesto tienen
-# las mismas claves que los diccionarios que representan a los
-# operacions.
+# Con estas funciones, se podrán hacer diferentes consultas de saldo
+# para mostrar el contenido del saldo en pantalla de diferentes formas.
 # -------------------------------------------------------------------
 
-
 # -------------------------------------------------------------------
-# Función para agregar un operacion al presupuesto de compras
+# Función para mostrar el contenido del saldo de compras
 # -------------------------------------------------------------------
-def agregar_al_presupuesto(codigo, monto):
+def mostrar_saldo():
     """
-    Agrega una operacion al presupuesto.
-
-    Parámetros:
-    - codigo: int, código numérico del operacion a agregar.
-    - tipo: int, tipo del operacion a agregar.
-
-    Retorna:
-    - bool: True si se pudo agregar el operacion al presupuesto, False si no se encontró el operacion o la tipo en stock es insuficiente.
+    Muestra en pantalla el contenido del saldo de compras.
     """
-    # Vemos si existe el operacion...
-    operacion = consultar_operacion(codigo)
-
-    # Si se devolvio un falso, el operacion no existe.
-    if operacion is False:
-        print("La operacion no existe.")
-        return False
-
+    # Creamos "acumuladores" e inicializamos en 0 cada variable
+    # para luego sumar allí sumar los montos de cada registros 
+    # según la categoría a la que pertenecen
+    ingresos_fijos= 0
+    ingresos_variables = 0
+    egresos_fijos =0
+    egresos_variables =0
+    ahorros = 0
+    saldo = 0
     
-    # Verificar si el operacion ya está en el presupuesto
-    for item in presupuesto:
-        if item['codigo'] == codigo:
-            # Si existe, sumo a la tipo del presupuesto...
-           item['monto'] += monto
-            # ...y descuento del stock de ese operacion.
-     #       operacion['monto'] -= tipo
-     #       return True
+    # Recorremos la lista de registros en el saldo
+    # y mostramos los datos de cada registro.
+    for registro in registros:
+        categoria = registro['categoria']
+        monto = registro['monto']
+        
+        categoria = registro['categoria']
+        monto = registro['monto']
 
-    # Si el operacion no está en el presupuesto, se agrega como un nuevo item
-    nuevo_item = {
-        'codigo': codigo,
-        'descripcion': operacion['descripcion'],
-        'tipo': operacion['tipo'],
-        'monto': monto
-    }
-    presupuesto.append(nuevo_item)
+        if categoria == 'Ingreso Fijo':
+            ingresos_fijos += monto
+        elif categoria == 'Ingreso Variable':
+            ingresos_variables += monto
+        elif categoria == 'Egreso Fijo':
+            egresos_fijos += monto
+        elif categoria == 'Egreso Variable':
+            egresos_variables += monto
+        elif categoria == 'Ahorro':
+            ahorros += monto               
 
-    # ...y descuento del stock de ese operacion.
-    operacion['monto'] += monto
-    return True
-
-
-# -------------------------------------------------------------------
-# Función para quitar un operacion del presupuesto de compras
-# -------------------------------------------------------------------
-def quitar_del_presupuesto(codigo, tipo):
-    """
-    Quita un operacion del presupuesto de compras.
-
-    Parámetros:
-    - codigo: int, código numérico del operacion a quitar.
-    - tipo: int, tipo del operacion a quitar.
-
-    Retorna:
-    - bool: True si se pudo quitar el operacion del presupuesto, False si no se encontró el operacion en el presupuesto o la tipo a quitar es mayor a la tipo en el presupuesto.
-    """
-    # Recorremos la lista de operacions en el presupuesto...
-    for item in presupuesto:
-        # Si se ecuentra ese operacion...
-        if item['codigo'] == codigo:
-            # Si no hay tipo suficiente en el presupuesto...
-            if tipo > item['tipo']:
-                print("tipo a quitar mayor a la tipo en el presupuesto.")
-                return False
-
-            # Si llegue aqui, es que puedo descontarlos del presupuesto...
-            item['tipo'] -= tipo
-
-            # ...y reponerlos en el stock de operacions!
-            operacion = consultar_operacion(codigo)
-            modificar_operacion(codigo, operacion["descripcion"], operacion["tipo"]-tipo, operacion["monto"] )
-
-            # Compruebo si la tipo de ese operacion en el presupuesto es cero:
-            if item['tipo'] == 0:
-                # Si quedo en cero, lo elimino del presupuesto.
-                presupuesto.remove(item)
-            return True
-
-    # Si el bucle finaliza sin novedad, es que ese operacion NO ESTA en el presupuesto.
-    print("El operacion no se encuentra en el presupuesto.")
-    return False
-
-
-# -------------------------------------------------------------------
-# Función para mostrar el contenido del presupuesto de compras
-# -------------------------------------------------------------------
-def mostrar_presupuesto():
-    """
-    Muestra en pantalla el contenido del presupuesto de compras.
-    """
-    # Inicializamos una variable para sumar los importes de cada item
-    suma = 0
+    saldo = ingresos_fijos + ingresos_variables - egresos_fijos - egresos_variables - ahorros
+    print("SALDOS")
+    print("-"*30)
+    print("Ingresos Fijos Totales:", ingresos_fijos)
+    print("Ingresos Variables Totales:", ingresos_variables)
+    print("Egresos Fijos Totales:", egresos_fijos)
+    print("Egresos Variables Totales:", egresos_variables)
+    print("Ahorros:", ahorros)
+    print("Saldo:", saldo)
     print("-"*30)
 
-    # Recorremos la lista de operacions en el presupuesto
-    # y mostramos los datos de cada operacion.
-    for item in presupuesto:
-        print(f'Cod: {item["codigo"]} - {item["descripcion"]}')
-        print(f'monto: {item["monto"]}  tipo: {item["tipo"]}')
-
-        # Calculamos el importe a pagar por el operacion...
-        importe = item["monto"] * item["tipo"]
-
-        # ...y acumulamos el importe en la variable suma.
-        suma += importe
-        print(f'Importe: {importe}')
-        print("-"*30)
-    print(f'Importe TOTAL: {suma}')
     return True
-
-
-
-
+    
 # -------------------------------------------------------------------
 # Ejemplo de uso de las funciones
 # -------------------------------------------------------------------
 
-# Agregamos operacions a la lista:
-agregar_operacion(1, 'Egreso','Alquiler', -150000)
-agregar_operacion(2, 'Ingreso','Salario', 450000)
-agregar_operacion(3, 'Egreso','Comida', -18000)
-agregar_operacion(4, 'Egreso','Entretenimiento', -10000)
-agregar_operacion(5, 'Ingreso','Clases', 10000)
-# eliminar_operacion(5) # Eliminamos un operacion del stock.
+# Agregamos registros (codigo, fecha, detalle, categoria, rubro, monto)
+agregar_registro(1, '01/06/2023', 'Alquiler' , 'Egreso Fijo', 'Vivienda', 100000)
+agregar_registro(2, '02/06/2023','Empleo' ,'Ingreso Fijo', 'Salario', 200000)
+agregar_registro(3, '12/06/2023','Proyecto Web' ,'Ingreso Variable', 'Facturación', 50000)
+agregar_registro(4, '13/06/2023','Carnicería', 'Egreso Variable', 'Alimentos', 10000)
+agregar_registro(5, '27/06/2023','Plazo Fijo', 'Ahorro', 'Banco', 10000)
+agregar_registro(6, '13/06/2023','Verdulería', 'Egreso Variable', 'Alimentos', 4000)
 
-# Consultar un operacion por su código
-# operacion = consultar_operacion(1)
-# if operacion:
-#     print(f"operacion encontrado: {operacion['descripcion']}")
-# else:
-#     print("operacion no encontrado.")
+# Eliminamos un registro 
+eliminar_registro(6)
 
-# Modificar un operacion por su código
-# modificar_operacion(1, 'Teclado mecánico 62 teclas', 20, 34000)
+# Consultar un registro por su código
+registro = consultar_registro(1)
+if registro:
+    print(f"Registro encontrado: {registro['detalle']}")
+else:
+    print("Registro no encontrado.")
 
-# Listamos todos los operacions en pantalla
-listar_operaciones()
-
-# Agregamos operaciones al presupuesto
-agregar_al_presupuesto(1, -15000)
-agregar_al_presupuesto(2, 45000)
-
-# agregar_al_presupuesto(30, 1) # intentamos agregar un operacion inexistente
-
-# Intentar agregar un operacion con tipo insuficiente
-# agregar_al_presupuesto(1, 150)
-
-# Quitar un operacion del presupuesto
-quitar_del_presupuesto(5, 10000)
-
-# Mostrar el contenido del presupuesto
-mostrar_presupuesto()
+# Modificar un registro por su código
+modificar_registro(3, '12/06/2023','Proyecto Web' ,'Ingreso Variable', 'Facturación', 75000)
+# Listamos todos los registros en pantalla
+listar_registros()
+# Mostrar el contenido del saldo
+mostrar_saldo()
