4bef3310f0e6215d0c37481327642790f79488a6
Bibiq <54219062+Bibiq@users.noreply.github.com>
2023-06-29 16:00:03 -0300

    Etapa 3 terminada

--- etapa3.py
+++ etapa3.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 # Configurar la conexión a la base de datos SQLite
-DATABASE = 'inventario.db'
+DATABASE = 'transacciones.db'
 
 # Obtener la conexión a la base de datos
 def get_db_connection():
@@ -9,15 +9,17 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-# Crear la tabla 'productos' si no existe
+# Crear la tabla 'registros' si no existe
 conn = get_db_connection()
 cursor = conn.cursor()
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS productos (
+    CREATE TABLE IF NOT EXISTS registros (
         codigo INTEGER PRIMARY KEY,
-        descripcion TEXT NOT NULL,
-        cantidad INTEGER NOT NULL,
-        precio REAL NOT NULL
+        fecha DATE NOT NULL,
+        detalle text NOT NULL,
+        categoria list NOT NULL,
+        rubro text NOT NULL,
+        monto real NOT NULL
     )
 ''')
 conn.commit()
@@ -55,125 +57,83 @@ class Transacciones:
 
     def agregar_registro(self, codigo, fecha, detalle, categoria, rubro, monto):
         new_registro = Registro (codigo, fecha, detalle, categoria, rubro, monto)
-        self.cursor.execute("INSERT INTO productos VALUES (?, ?, ?, ?, ?, ?)", (codigo, fecha, detalle, categoria, rubro, monto))
+        self.cursor.execute("INSERT INTO registros VALUES (?, ?, ?, ?, ?, ?)", (codigo, fecha, detalle, categoria, rubro, monto))
         self.conexion.commit()
         return True
 
     def consultar_registro(self, codigo):
-        self.cursor.execute("SELECT * FROM productos WHERE codigo = ?", (codigo,))
+        self.cursor.execute("SELECT * FROM registros WHERE codigo = ?", (codigo,))
         row = self.cursor.fetchone()
         if row:
             codigo, fecha, detalle, categoria, rubro, monto = row
             return Registro(codigo, fecha, detalle, categoria, rubro, monto)
         return False
 
-    def modificar_producto(self, codigo,new_fecha, new_detalle, new_categoria, new_rubro, new_monto):
-        registro = self.consultar_producto(codigo)
+    def modificar_registro(self, codigo,new_fecha, new_detalle, new_categoria, new_rubro, new_monto):
+        registro = self.consultar_registro(codigo)
         if registro:
             registro.modificar(new_fecha, new_detalle, new_categoria, new_rubro, new_monto)
-            self.cursor.execute("UPDATE productos SET descripcion = ?, cantidad = ?, precio = ? WHERE codigo = ?",
+            self.cursor.execute("UPDATE registros SET fecha = ?, detalle= ?, categoria = ?, rubro = ?, monto = ? WHERE codigo = ?",
                                 (new_fecha, new_detalle, new_categoria, new_rubro, new_monto, codigo))
             self.conexion.commit()
 
-    def listar_productos(self):
+    def listar_registros(self):
         print("-" * 30)
-        self.cursor.execute("SELECT * FROM productos")
+        self.cursor.execute("SELECT * FROM registros")
         rows = self.cursor.fetchall()
         for row in rows:
-            codigo, descripcion, cantidad, precio = row
+            codigo, fecha, detalle, categoria, rubro, monto = row
             print(f"Código: {codigo}")
-            print(f"Descripción: {descripcion}")
-            print(f"Cantidad: {cantidad}")
-            print(f"Precio: {precio}")
+            print(f"Fecha: {fecha}")
+            print(f"Detalle: {detalle}")
+            print(f"Categoría: {categoria}")
+            print(f"Rubro: {rubro}")
+            print(f"Monto: {monto}")
             print("-" * 30)
 
-    def eliminar_producto(self, codigo):
-        self.cursor.execute("DELETE FROM productos WHERE codigo = ?", (codigo,))
+    def eliminar_registro(self, codigo):
+        self.cursor.execute("DELETE FROM registros WHERE codigo = ?", (codigo,))
         if self.cursor.rowcount > 0:
-            print("Producto eliminado.")
+            print("Registro eliminado.")
             self.conexion.commit()
         else:
-            print("Producto no encontrado.")
-
+            print("Registro no encontrado.")
 
 # -------------------------------------------------------------------
-# Definimos la clase "Carrito"
+# Definimos la clase "Saldo"
 # -------------------------------------------------------------------
-class Carrito:
+class Saldo:
     def __init__(self):
-        self.conexion = sqlite3.connect('inventario.db')  # Conexión a la base de datos
+        self.conexion = sqlite3.connect('transacciones.db')  # Conexión a la base de datos
         self.cursor = self.conexion.cursor()
-        self.items = []
-
-    def agregar(self, codigo, cantidad, inventario):
-        producto = inventario.consultar_producto(codigo)
-        if producto is False:
-            print("El producto no existe.")
-            return False
-        if producto.cantidad < cantidad:
-            print("Cantidad en stock insuficiente.")
-            return False
-
-        for item in self.items:
-            if item.codigo == codigo:
-                item.cantidad += cantidad
-                self.cursor.execute("UPDATE productos SET cantidad = cantidad - ? WHERE codigo = ?",
-                                    (cantidad, codigo))
-                self.conexion.commit()
-                return True
-
-        nuevo_item = Producto(codigo, producto.descripcion, cantidad, producto.precio)
-        self.items.append(nuevo_item)
-        self.cursor.execute("UPDATE productos SET cantidad = cantidad - ? WHERE codigo = ?",
-                            (cantidad, codigo))
-        self.conexion.commit()
-        return True
-
-    def quitar(self, codigo, cantidad, inventario): #Revisar
-        for item in self.items:
-            if item.codigo == codigo:
-                if cantidad > item.cantidad:
-                    print("Cantidad a quitar mayor a la cantidad en el carrito.")
-                    return False
-                item.cantidad -= cantidad
-                if item.cantidad == 0:
-                    self.items.remove(item)
-                self.cursor.execute("UPDATE productos SET cantidad = cantidad + ? WHERE codigo = ?",
-                                    (cantidad, codigo))
-                self.conexion.commit()
-                return True
-
-        print("El producto no se encuentra en el carrito.")
-        return False
+        self.operaciones = []
 
     def mostrar(self):
         print("-" * 30)
-        for item in self.items:
-            print(f"Código: {item.codigo}")
-            print(f"Descripción: {item.descripcion}")
-            print(f"Cantidad: {item.cantidad}")
-            print(f"Precio: {item.precio}")
+        for operacion in self.operaciones:
+            print(f"Código: {operacion['codigo']}")
+            print(f"Fecha: {operacion['fecha']}")
+            print(f"Detalle: {operacion['detalle']}")
+            print(f"Categoria: {operacion['categoria']}")
+            print(f"Rubro: {operacion['rubro']}")
+            print(f"Monto: {operacion['monto']}")
             print("-" * 30)
-
 
 # -------------------------------------------------------------------
 # Ejemplo de uso de las clases y objetos definidos antes:
 # -------------------------------------------------------------------
-# Crear una instancia de la clase Inventario
-x = Inventario()
+# Crear una instancia de la clase Transaccion
+x = Transacciones()
 
-# Crear una instancia de la clase Carrito
-mi_carrito = Carrito()
+# Crear una instancia de la clase Saldo
+mi_saldo = Saldo()
 
-# Agregar productos al inventario
-x.agregar_producto(1, "Producto 1", 10, 19.99)
-x.agregar_producto(2, "Producto 2", 5, 9.99)
-x.agregar_producto(3, "Producto 3", 15, 29.99)
+# Agregar registros al inventario
+x.agregar_registro(1, '01/06/2023', 'Alquiler' , 'Egreso Fijo', 'Vivienda', 100000)
+x.agregar_registro(2, '02/06/2023','Empleo' ,'Ingreso Fijo', 'Salario', 200000)
+x.agregar_registro(3, '12/06/2023','Proyecto Web' ,'Ingreso Variable', 'Facturación', 50000)
+x.agregar_registro(4, '13/06/2023','Carnicería', 'Egreso Variable', 'Alimentos', 10000)
+x.agregar_registro(5, '27/06/2023','Plazo Fijo', 'Ahorro', 'Banco', 10000)
+x.agregar_registro(6, '27/06/2023','Plazo Fijo', 'Ahorro', 'Banco', 10000)
 
-# Agregar productos al carrito
-mi_carrito.agregar(1, 2, x)  # Agregar 2 unidades del producto con código 1 al carrito
-mi_carrito.agregar(3, 1, x)  # Agregar 1 unidad del producto con código 3 al carrito
-mi_carrito.quitar(1, 1, x)   # Quitar 1 unidad del producto con código 1 al carrito
-mi_carrito.agregar(2, 1, x)  # Agregar 1 unidad del producto con código 2 al carrito
-
-mi_carrito.mostrar()
+mi_saldo.mostrar()
